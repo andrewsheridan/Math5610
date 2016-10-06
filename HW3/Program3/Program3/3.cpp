e0b7f332c55fdd36ae6698906bf4c265193784b3
Andrew Home Computer <sheridan.andrew13@gmail.com>
2016-10-06 16:02:23 -0600

    Updates.

--- HW3/Program3/Program3/3.cpp
+++ HW3/Program3/Program3/3.cpp
@@ -26,12 +26,24 @@ double dg(double x) {
 	return 0.5 * sinh(x / 4) - 1;
 }
 
+double pten(double x) {
+	return (x - 1)*(x - 1)*std::exp(x);
+}
+
+double ptenprime(double x) {
+	return std::exp(x) * (x - 1) * (x + 1);
+}
+
 int main(void) {
 	double result = newton(f, df, 1, 0.0000001, 10);
 	std::cout << "result: " << result << std::endl;
 
 	double result2 = newton(g, dg, 2, pow(10, -8), 10);
 	std::cout << "result2: " << result2 << std::endl;
+
+	double problem10 = newton(pten, ptenprime, 2, pow(10, -8), 15);
+	std::cout << "problem10: " << problem10 << std::endl;
 	std::cin >> result;
+
 	return 0;
 }
--- HW3/Program3/Program3/Newton_Method.h
+++ HW3/Program3/Program3/Newton_Method.h
@@ -3,16 +3,19 @@
 // 09/26/15
 
 //Newton_Method.h
+//Written in C++
 #pragma once
 #include <cmath>
 
 double newton(double (*f)(double), double (*df)(double), double x0, double tol, unsigned max_iterations) {
 	double xk = 99999999; //x sub k
 	double xkp1 = x0; //x sub k+1
+	std::cout << "x_kp1 \t x_k" << std::endl;
 	//Loops until we reach the maximum iterations or we are within the input tolerance.
 	for (unsigned k = 0; k < max_iterations && std::abs(xkp1 - xk) > tol; k++) {
 		xk = xkp1;
 		xkp1 = xk - (f(xk) / df(xk));
+		std::cout << xkp1 << "\t" << xk << std::endl;
 	}
 
 	return xkp1;
