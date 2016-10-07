e647f051aedfcd66741a35cc763a7d97a55e2168
Andrew Home Computer <sheridan.andrew13@gmail.com>
2016-10-06 18:07:05 -0600

    Back Substition complete.

--- HW4/BackSubstitution/BackSubstitution/Main.cpp
+++ HW4/BackSubstitution/BackSubstitution/Main.cpp
@@ -9,7 +9,51 @@
 
 
 int main(void) {
+	const unsigned size = 3;
 
+	//Problem 1
+	double** matrix = CreateUpperTriangularMatrix(size);
+	double* vector = CreateOnesVector(size);
+	vector = VectorMatrixMultiply(matrix, vector, size);
+	double** matrixCopy = CopyMatrix(matrix, size);
+	double* result = BackSubstitution(matrixCopy, vector, size);
+
+	std::cout << "Problem 1" << std::endl;
+	std::cout << "Upper triangular matrix" << std::endl;
+	PrintMatrix(matrix, size);
+	std::cout << "Test vector " << std::endl;
+	PrintVector(vector, size);
+	std::cout << "Result of back substition " << std::endl;
+	PrintVector(result, size);
+
+
+	//Problem 5
+	
+	matrix = CreateMatrix(size);
+	vector = new double[size];
+	vector[0] = 1;
+	vector[1] = 2;
+	vector[2] = 3;
+
+
+	double* fivePartOne = VectorMatrixMultiply(matrix, vector, size);
+	std::cout << "Problem 5" << std::endl;
+	std::cout << "5.1" << std::endl;
+	PrintMatrix(matrix, size);
+	PrintVector(vector, size);
+	PrintVector(fivePartOne, size);
+
+
+	double* onesVector = CreateOnesVector(size);
+	double* fivePartThree = VectorMatrixMultiply(matrix, onesVector, size);
+
+	std::cout << "Results from 5.3" << std::endl;
+	PrintVector(onesVector, size);
+	PrintVector(fivePartThree, size);
+
+	//Stop console from closing
+	int input;
+	std::cin >> input;
 
 	return 0;
 }
--- HW4/BackSubstitution/BackSubstitution/Matrix.h
+++ HW4/BackSubstitution/BackSubstitution/Matrix.h
@@ -18,7 +18,7 @@ double* BackSubstitution(double **A, double *b, unsigned n) {
 	x = new double[n];
 	try {
 		x[n - 1] = b[n - 1] / A[n - 1][n - 1];
-		for (unsigned k = n - 2; k >= 0; k--) {
+		for (int k = n - 2; k >= 0; k--) {
 			x[k] = b[k];
 			for (int i = k + 1; i < n; i++) {
 				x[k] -= A[k][i] * x[i];
@@ -29,52 +29,192 @@ double* BackSubstitution(double **A, double *b, unsigned n) {
 	catch (std::exception& e)
 	{
 		std::cout << "These matrices are not the correct size." << std::endl;
-		return new double[];
+		return new double[n];
 	}
 
 	return x;
 }
 
-/// Solves a set of linear equations using forward substitution
-// A: The nxn lower-triangular coefficient matrix
-// b: Right-Hand-Side
-// n: The size of the matrices
-double* forwardSubstitution(double** A, double* b, unsigned n) {
+ /// Solves a set of linear equations using forward substitution
+ //a: the nxn lower-triangular coefficient matrix
+ //b: right-hand-side
+ //n: the size of the matrices
+double* forwardsubstitution(double** A, double* b, unsigned n) {
+	double* x;
+	x = new double[n];
+	try {
+		x[0] = b[0];
+		for (int k = 1; k < n; k++) {
+			x[k] = b[k];
+			for(int j = 1; j < k-1; j++){
+				x[k] = x[k] - A[k][j] * x[j];
+			}
+		}
+	}
+	catch (std::exception& e)
+	{
+		std::cout << "These matrices are not the correct size." << std::endl;
+		return new double[n];
+	}
 
-
+	return x;
 }
 
 /// Reduces an nxn matrix A and right-hand-side b to upper-triangular form using Gaussian Elimination
 // A: The nxn coefficient matrix
 // b: Right-Hand-Side
 // n: The size of the matrices
-double** GaussianElimination(double** A, double* b, unsigned n) {
-
-}
+//double** GaussianElimination(double** A, double* b, unsigned n) {
+//
+//}
 
 /// Generates a random square matrix of size n
 // n: The size of the matrix
-double** GenerateMatrix(unsigned n) {
+double** CreateMatrix(unsigned n) {
 	std::mt19937 generator(123); //Random number generator
 	std::uniform_real_distribution<double> dis(0.0, 1.0); //Desired distribution
 	
 	double** matrix;
 	matrix = new double *[n];
-	for (int i = 0; i < n; i++) {
+	for (unsigned i = 0; i < n; i++) {
 		matrix[i] = new double [n];
-		for (int j = 0; j < n; j++) {
+		for (unsigned j = 0; j < n; j++) {
 			matrix[i][j] = dis(generator); //Assign each entry in matrix to random number between 0 and 1
 		}
 	}
 
-	for (int k = 0; k < n; k++) {
+	for (unsigned k = 0; k < n; k++) {
 		matrix[k][k] += 10*n; //Add 10*n to all diagonal entries
 	}
 
 	return matrix;
 }
 
+/// Generates a random upper triangular square matrix of size n
+// n: The size of the matrix
+double** CreateUpperTriangularMatrix(unsigned n) {
+	std::mt19937 generator(123); //Random number generator
+	std::uniform_real_distribution<double> dis(0.0, 1.0); //Desired distribution
 
+	double** matrix;
+	matrix = new double *[n];
+	for (unsigned i = 0; i < n; i++) {
+		matrix[i] = new double[n];
+		for (unsigned j = 0; j < n; j++) {
+			if (j < i) {
+				matrix[i][j] = 0; 
+			}
+			else {
+				matrix[i][j] = dis(generator); //Assign entry in matrix to random number between 0 and 1
+			}
+		}
+	}
+
+	for (unsigned k = 0; k < n; k++) {
+		matrix[k][k] += 10 * n; //Add 10*n to all diagonal entries
+	}
+
+	return matrix;
+}
+
+/// Generates a random lower triangular square matrix of size n
+// n: The size of the matrix
+double** CreateLowerTriangularMatrix(unsigned n) {
+	std::mt19937 generator(123); //Random number generator
+	std::uniform_real_distribution<double> dis(0.0, 1.0); //Desired distribution
+
+	double** matrix;
+	matrix = new double *[n];
+	for (unsigned i = 0; i < n; i++) {
+		matrix[i] = new double[n];
+		for (unsigned j = 0; j < n; j++) {
+			if (i < j) {
+				matrix[i][j] = 0;
+			}
+			else {
+				matrix[i][j] = dis(generator); //Assign entry in matrix to random number between 0 and 1
+			}
+		}
+	}
+
+	for (unsigned k = 0; k < n; k++) {
+		matrix[k][k] += 10 * n; //Add 10*n to all diagonal entries
+	}
+
+	return matrix;
+}
+
+/// Generates a random vector of size n
+// n: The size of the vector
+double* CreateVector(unsigned n) {
+	std::mt19937 generator(123); //Random number generator
+	std::uniform_real_distribution<double> dis(0.0, 1.0); //Desired distribution
+
+	double* vector;
+	vector = new double [n];
+	for (unsigned i = 0; i < n; i++) {
+		vector[i] = dis(generator); //Assign each entry in vector to random number between 0 and 1
+	}
+
+	return vector;
+}
+
+///Multiplies an nxn matrix A by the vector x
+double* VectorMatrixMultiply(double** A, double* x, unsigned n) {
+	double* result = new double[n];
+	try {
+		for (unsigned i = 0; i < n; i++) {
+			result[i] = 0;
+			for (unsigned j = 0; j < n; j++) {
+				result[i] += A[i][j] * x[j];
+			}
+		}
+		return result;
+	}
+	catch (std::exception& e) {
+		std::cout << "These matrices are not the correct size." << std::endl;
+	}
+}
+
+///Creates a vector of all ones of size n
+double* CreateOnesVector(unsigned n) {
+	double* vector = new double[n];
+	for (unsigned i = 0; i < n; i++) {
+		vector[i] = 1;
+	}
+	return vector;
+}
+
+///Outputs an nxn matrix to the console
+void PrintMatrix(double** matrix, unsigned size) {
+	for (unsigned i = 0; i < size; i++) {
+		for (unsigned j = 0; j < size; j++) {
+			std::cout << matrix[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+}
+
+///Outputs a size n vector to the console
+void PrintVector(double* vector, unsigned size) {
+	for (unsigned i = 0; i < size; i++) {
+		std::cout << vector[i] << " ";
+	}
+	std::cout << std::endl << std::endl;
+}
+
+///Returns a copy of the input nxn matrix
+double** CopyMatrix(double** matrix, unsigned size) {
+	double** result = new double*[size];
+	for (unsigned i = 0; i < size; i++) {
+		result[i] = new double[size];
+		for (unsigned j = 0; j < size; j++) {
+			result[i][j] = matrix[i][j];
+		}
+	}
+	return result;
+}
 
 //bool checkArraySize(double *A, unsigned size) {
 //	if (sizeof((A)) / sizeof((A[0])) == size)
