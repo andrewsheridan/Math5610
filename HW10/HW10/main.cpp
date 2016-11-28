49c51a1fe4318b1481730dd977bc28b6ecd9d175
A01488483 <A01488483@ENG30139>
2016-11-28 10:45:42 -0700

    HW10 Setup.

---
+++ HW10/HW10/main.cpp
@@ -0,0 +1,10 @@
+#include "../../HW7/HW7/Matrix.h";
+#include "../../HW7/HW7/MatrixFactory.h";
+#include "../../HW7/HW7/MatrixOperations.h";
+#include "../../HW7/HW7/Vector.h";
+
+int main() {
+	//problem 1: Power Method
+
+	return 0;
+}
